98d84248d0e4b0f7ec910527c6df2c81057b48da
Preston <prestoncam.campbell@digipen.edu>
2019-06-28 11:32:49 -0700

    Updated

---
+++ DK Remake/Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public KeyCode up = KeyCode.W;
+    public KeyCode down = KeyCode.S;
+    public KeyCode left = KeyCode.A;
+    public KeyCode right = KeyCode.D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(left))
+        {
+            Debug.Log("its time for lunch time");
+        }
+    }
+}
