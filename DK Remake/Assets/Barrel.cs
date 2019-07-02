766777d79fe781e7ea80d4f8ad52812059624f0f
Preston <prestoncam.campbell@digipen.edu>
2019-07-02 16:05:38 -0700

    Barrel Paths

--- DK Remake/Assets/Barrel.cs
+++ DK Remake/Assets/Barrel.cs
@@ -9,18 +9,21 @@ using UnityEngine;
 public class Barrel : MonoBehaviour
 {
     Rigidbody rb;
-    public float speed = -3;
+    public float speed = -6;
     bool CoolDown;
 
     // Start is called before the first frame update
     void Start()
     {
+        CoolDown = true;
         rb = GetComponent<Rigidbody>();
     }
     bool OnGround()
     {
         CoolDown = true;
-        return Physics.Raycast(transform.position, Vector3.down, transform.localScale.y / 2f + 0.1f);
+        return Physics.Raycast(transform.position, Vector3.down, transform.localScale.y / 2f + 0.1f)
+            || Physics.Raycast(transform.position + Vector3.right / 2f, Vector3.down, transform.localScale.y / 2f + 0.1f)
+            || Physics.Raycast(transform.position + Vector3.left / 2f, Vector3.down, transform.localScale.y / 2f + 0.1f);
     }
     void Death()
     {
@@ -43,6 +46,22 @@ public class Barrel : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        float dy = other.transform.position.y - transform.position.y;
+        if (dy < -2.5f)
+        {
+            if (Random.Range(0f, 2f) >= 1)
+            {
+                float x = transform.position.x;
+                float y = transform.position.y - 2;
+                float z = transform.position.z;
+                rb.velocity = new Vector3(0, rb.velocity.y, 0);
+                transform.position = new Vector3(x,y,z);
+            }
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -59,6 +78,7 @@ public class Barrel : MonoBehaviour
         if (!OnGround())
         {
             CoolDown = false;
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
         }
     }
     private void OnDrawGizmos()
--- DK Remake/Assets/PlayerController.cs
+++ DK Remake/Assets/PlayerController.cs
@@ -8,13 +8,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public GameObject spawner;
-    public KeyCode up = KeyCode.W;
+    public KeyCode up = KeyCode.Space;
     public KeyCode down = KeyCode.S;
     public KeyCode left = KeyCode.A;
     public KeyCode right = KeyCode.D;
+    public KeyCode climb = KeyCode.W;
     public float jumpspeed = 5;
-    public float speed = 5;
+    public float ladderspeed = 3;
+    public float speed = 3;
     Rigidbody rb;
+    float save;
+    bool Ladder;
+    GameObject collide;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,21 +37,83 @@ public class PlayerController : MonoBehaviour
             }
         }
     }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Ladder" && Ladder == false && (Input.GetKey(climb) || Input.GetKey(down)))
+        {
+            Ladder = true;
+            collide = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Ladder" && Ladder == true)
+        {
+            Ladder = false;
+            Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Platform"), false);
+        }
+    }
+
     bool OnGround()
     {
         return Physics.Raycast(transform.position, Vector3.down, transform.localScale.y / 2f + 0.1f);
     }
+
+    void ladder()
+    {
+        if (Ladder)
+        {
+             Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Platform"), true);
+            rb.useGravity = false;
+            float x = collide.transform.position.x;
+            float y = transform.position.y;
+            float z = transform.position.z;
+            Vector3 newVel = Vector3.zero;
+            newVel.y = ladderspeed;
+            newVel.x = 0;
+            newVel.z = 0;
+
+            if (Input.GetKey(climb) || (Input.GetKey(down)))
+            {
+                if (Input.GetKey(climb))
+                {
+                    rb.velocity = newVel;
+                    transform.position = new Vector3(x, y, z);
+                }
+                if (Input.GetKey(down))
+                {
+                    rb.velocity = -newVel;
+                    transform.position = new Vector3(x, y, z);
+                }
+            }
+            else
+            {
+                if (!Input.GetKey(left) && !Input.GetKey(right))
+                    rb.velocity = Vector3.zero;
+            }
+        }
+        else
+        {
+            rb.useGravity = true;
+            Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Platform"), false);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
+        ladder();
         Death();
         Vector3 newVel = rb.velocity;
         if (Input.GetKey(left))
         {
+            Ladder = false;
             newVel.x = -speed;
         }
         if (Input.GetKey(right))
         {
+            Ladder = false;
             newVel.x = speed;
         }
         if (Input.GetKeyDown(up) && OnGround())
