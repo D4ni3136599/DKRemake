d1c014641c9a8b27a38fbdb0b06ce59b9e42c60c
Liam <liam.murray@digipen.edu>
2019-07-03 16:01:53 -0700

    Added DK Animation

---
+++ DK Remake/Assets/MyAnimaton.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyAnimaton : MonoBehaviour
+{
+    public Sprite[] sprites;
+    int currentSprite = 0;
+    public float framesPerSecond;
+    float secondsPerFrame;
+    float timeUntilChange;
+
+    // Start is called before the first frame update
+    void UpdateSecondsPerFrame()
+    {
+        if (framesPerSecond == 0)
+            framesPerSecond = 0.0001f;
+        secondsPerFrame = 1f / Mathf.Abs(framesPerSecond);
+    }
+    private void Start()
+    {
+        UpdateSecondsPerFrame();
+        timeUntilChange = secondsPerFrame;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateSecondsPerFrame();
+        GetComponent<SpriteRenderer>().sprite = sprites[currentSprite];
+        timeUntilChange -= Time.deltaTime;
+        if (framesPerSecond > 0 && timeUntilChange < 0)
+        {
+            timeUntilChange = secondsPerFrame;
+            currentSprite++;
+            if (currentSprite >= sprites.Length)
+                currentSprite = 0;
+        }
+        if(framesPerSecond<0 && timeUntilChange<0)
+        {
+            timeUntilChange = secondsPerFrame;
+            currentSprite--;
+            if (currentSprite <0)
+                currentSprite=sprites.Length-1;
+        }
+            
+    }
+}
